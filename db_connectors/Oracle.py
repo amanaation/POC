31cc5ad0e80abb40def0e8362c8e6de2fe176467
amanaation-zyte <iamanmishra2007@gmail.com>
2023-02-12 22:17:40 +0530

    Adding dynamic limit

--- db_connectors/Oracle.py
+++ db_connectors/Oracle.py
@@ -6,8 +6,8 @@ import json
 
 from dotenv import load_dotenv
 from sqlalchemy import create_engine
-from db_connectors.connectors import Connectors
-# from connectors import Connectors
+# from db_connectors.connectors import Connectors
+from connectors import Connectors
 from pprint import pprint
 
 logging.basicConfig(format='%(asctime)s,%(msecs)03d %(levelname)-8s [%(filename)s:%(lineno)d] %(message)s',
@@ -68,6 +68,7 @@ class OracleDatabaseConnection(Connectors):
         return schema_details
 
     def get_incremental_clause(self, incremental_columns: dict, last_successfull_extract: dict) -> str:
+
         """
         Creates the inmcremental clause to be added to where clause to fetch latest data
 
@@ -123,40 +124,78 @@ class OracleDatabaseConnection(Connectors):
         result = pd.read_sql(sql, self.connection)
         return result
 
-    def execute_batch(self, query: str, batch_size: int, incremental_columns: dict) -> pd.DataFrame :
-        """
-        Execute the query and fetch the data in batches using the given batch size
+    def add_dynamic_limit(self, 
+                          incremental_clause, 
+                          table_name, 
+                          batch_size, 
+                          group_by_column, 
+                          group_by_format):
 
-        Parameters
-        ------------
-            query: str
-                Query to be executed
-            batch_size: int
-                size of batch in which the data should be fetched
-            incremental_columns: dict
-                Incremental column details
+        dynamic_limit = self.get_dynamic_limit(incremental_clause, table_name, batch_size, group_by_column, group_by_format)
+        print(dynamic_limit)
 
-        Returns
-        -----------
-        pd.DataFrame: Result Dataframe
-        """
+    def create_dynamic_limit_query(self, incremental_clause, table_name, batch_size, group_by_column, group_by_format):
+        if incremental_clause:
+            dynamic_limit_query = """select TRUNC({}, '{}') as group_by_timestamp, 
+                                    count(*) as row_count from {} where {} group by TRUNC({}, '{}')  
+                                    order by group_by_timestamp desc; """
+            dynamic_limit_query = dynamic_limit_query.format(group_by_column, group_by_format, table_name, incremental_clause, group_by_column,  group_by_format)
+
+        else:
+            dynamic_limit_query = """select TRUNC({}, '{}') as group_by_timestamp, 
+                                    count(*) as row_count from {} group by TRUNC({}, '{}')  
+                                    order by group_by_timestamp desc; """
+
+            dynamic_limit_query = dynamic_limit_query.format(group_by_column, group_by_format, table_name, group_by_column,  group_by_format)
+
+        return dynamic_limit_query
+
+    def get_dynamic_limit(self, incremental_clause, table_name, batch_size, group_by_column, group_by_format):
+        group_by_formats = ["hh", "dd", "mm", "yy"]
+
+        for i in range(len(group_by_formats)):
+            group_by_format = group_by_formats[i]
+            dynamic_limit_query = self.create_dynamic_limit_query( incremental_clause, table_name, batch_size, group_by_column, group_by_format)
+
+            result = self.execute_query(dynamic_limit_query)
+            if result["group_by_timestamp"][0] >= batch_size:
+                if i:
+                    i = i-1
+                break
+        group_by_format = group_by_formats[i]
+        final_dynamic_limit_query = self.create_dynamic_limit_query( incremental_clause, table_name, batch_size, group_by_column, group_by_format)
+        result = self.execute_query(final_dynamic_limit_query)
+
+        return result
+
+    def execute_batches(self, 
+                        query: str, 
+                        incremental_clause, 
+                        table_name,
+                        batch_size: int, 
+                        incremental_columns: dict, 
+                        group_by_column, 
+                        group_by_format) -> pd.DataFrame :
+
         offset = 0
         result_df = pd.DataFrame()
         incremental_columns = list(incremental_columns.keys())
         incremental_columns = ', '.join(incremental_columns)
+        print("incremental_columns : ", incremental_columns)
 
-        while True:
-            logger.info(f"Fetching rows between {offset} and {offset + batch_size}")
-            updated_query = query + f" order by {incremental_columns} " + f" OFFSET {offset} ROWS FETCH NEXT {batch_size} ROWS ONLY"
-            pd_result = self.execute_query(updated_query)
-            yield pd_result
+        yield self.get_dynamic_limit(incremental_clause, table_name, batch_size, group_by_column, group_by_format)
+        # while True:
+        #     logger.info(f"Fetching rows between {offset} and {offset + batch_size}")
+        #     updated_query = query + f" order by {incremental_columns} " + f" OFFSET {offset} ROWS FETCH NEXT {batch_size} ROWS ONLY"
+        #     pd_result = self.execute_query(updated_query)
+        #     yield pd_result
 
-            result_df = pd.concat([result_df, pd_result], ignore_index=True)
-            offset += batch_size
-            if len(pd_result) < batch_size:
-                break
+        #     result_df = pd.concat([result_df, pd_result], ignore_index=True)
+        #     offset += batch_size
+        #     if len(pd_result) < batch_size:
+        #         break
         
-        return 
+        # return 
 
     def handle_extract_error(self):
         pass
@@ -190,6 +229,7 @@ class OracleDatabaseConnection(Connectors):
             --------
                 pd.DataFrame : Extracted dataframe from source table
         """
+
         if last_successfull_extract:
             self.last_successfull_extract = last_successfull_extract
 
@@ -198,6 +238,7 @@ class OracleDatabaseConnection(Connectors):
         incremental_columns = table["incremental_column"]
 
         # If there is a last successfull extract then add incremental clause
+        incremental_clause = ""
         if last_successfull_extract:
             incremental_clause = self.get_incremental_clause(incremental_columns, last_successfull_extract)
 
@@ -212,38 +253,70 @@ class OracleDatabaseConnection(Connectors):
         if not table["use_offset"]:
             yield self.execute_query(query)
         else:
-            try:
-                func = self.execute_batch(query, batch_size, incremental_columns)
+            # try:
+                print("batch execution")
+                func = self.execute_batches(query, incremental_clause, table["name"], batch_size, incremental_columns, table["grouby_column"], table["grouby_format"])
                 while True:
                     return_args["extraction_status"] = True
+                    print("batch execution------")
+
                     result_df = next(func)
-                    print("Updating values after extract")
 
                     self.update_last_successfull_extract(incremental_columns, result_df)
                     yield result_df, return_args 
-            except StopIteration:
-                pass
-            except Exception as e:
-                yield pd.DataFrame(), return_args 
+            # except StopIteration:
+            #     pass
+            # except Exception as e:
+            #     yield pd.DataFrame(), return_args 
 
 
-"""
-# if __name__ == "__main__":
-#     import os
-#     conn_details = {"user": os.getenv("DBUSER"),
-#                     "password": os.getenv("PASSWORD"),
-#                     "host": os.getenv("HOST"),
-#                     "port": os.getenv("PORT"),
-#                     "DB": os.getenv("DB")
-#                     }
+# """
+if __name__ == "__main__":
+    import os
+    conn_details = {"user": os.getenv("DBUSER"),
+                    "password": os.getenv("PASSWORD"),
+                    "host": os.getenv("HOST"),
+                    "port": os.getenv("PORT"),
+                    "DB": os.getenv("DB")
+                    }
 
-#     pprint(conn_details)
-#     last_extract_value = {"tdate": "2022-04-24 00:00:00", "meantemp": "135"}
-#     last_extract_value = None
+    pprint(conn_details)
+    # last_extract_value = {"tdate": "2022-04-24 00:00:00", "meantemp": "135"}
+    last_extract_value = None
 
-#     table = {'name': 'climate', 'extract': True, 'source': 'oracle', 'source_type': 'db', 'destination': 'bq', 'timestamp_column': 'tdate', 'timestamp_format': 'YYYY-MM-DD HH24:MI:SS', 'incremental_column': {'tdate': {'column_type': 'timestamp', 'column_format': 'YYYY-MM-DD HH24:MI:SS'}, 'meantemp': {'column_type': 'id'}}, 'incremental_type': 'timestamp', 'incremental_column_format': 'YYYY-MM-DD HH24:MI:SS', 'frequency': 'daily', 'query': 'select * from climate', 'batch_size': 300, 'use_offset': True, 'gcp_project_id': 'turing-nature-374608', 'gcp_bq_dataset_name': 'test_dataset2', 'target_project_id': 'turing-nature-374608', 'target_bq_dataset_name': 'test_dataset2', 'target_table_name': 'test_climate_bq2', 'target_operation': 'a'}
+    table = {'name': 'climate', 
+            'extract': True, 
+            'source': 'oracle', 
+            'source_type': 'db', 
+            'destination': 'bq', 
+            'timestamp_column': 'tdate', 
+            'timestamp_format': 'YYYY-MM-DD HH24:MI:SS', 
 
-#     odb = OracleDatabaseConnection(**conn_details)
+            'grouby_column': 'tdate', 
+            'grouby_format': 'YY', 
+
+            'incremental_column': {'tdate': {'column_type': 'timestamp', 'column_format': 'YYYY-MM-DD HH24:MI:SS'}, 'meantemp': {'column_type': 'id'}}, 
+            'incremental_type': 'timestamp', 
+            'incremental_column_format': 'YYYY-MM-DD HH24:MI:SS', 
+            'frequency': 'daily', 
+            'query': 'select * from climate', 
+            'batch_size': 300, 
+            'use_offset': True, 
+            'gcp_project_id': 'turing-nature-374608', 
+            'gcp_bq_dataset_name': 'test_dataset2', 
+            'target_project_id': 'turing-nature-374608', 
+            'target_bq_dataset_name': 'test_dataset2', 
+            'target_table_name': 'test_climate_bq2', 
+            'target_operation': 'a'}
+
+    odb = OracleDatabaseConnection(**conn_details)
+
+    # odb.execute_batch("select * from climate", 500, [{"tdate": {"column_type":"timestamp", "column_format": 'YYYY-MM-DD HH24:MI:SS'}}], "tdate", "DD")
+    # odb.test()
+    # odb.execute_batch("select * from climate", 500, [{"tdate": {"column_type":"timestamp", "column_format": 'YYYY-MM-DD HH24:MI:SS'}}], "tdate", "DD")
+    next(odb.extract(last_extract_value, **table))
+    print("-----------")
+
 #     func = odb.extract(last_extract_value, **table)
 
 #     try:
@@ -254,4 +327,4 @@ class OracleDatabaseConnection(Connectors):
 #     except StopIteration:
 #         pass
         
-"""
+# """
